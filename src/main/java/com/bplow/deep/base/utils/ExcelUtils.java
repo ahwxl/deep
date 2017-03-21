a2f14cd8fba6e09d3682ef287c38c5149a03bc1e
wangxiaolei <wangxiaolei@shfft.com>
2017-03-21 17:52:15 +0800

    补充

--- src/main/java/com/bplow/deep/base/utils/ExcelUtils.java
+++ src/main/java/com/bplow/deep/base/utils/ExcelUtils.java
@@ -1,7 +1,10 @@
 package com.bplow.deep.base.utils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.util.CellReference;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
@@ -11,42 +14,175 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.taobao.api.internal.toplink.embedded.websocket.util.StringUtil;
+
 public class ExcelUtils {
-    
-    public void read() throws InvalidFormatException, IOException{
-        
-        OPCPackage pkg = OPCPackage.open("D:/www/work/G-光大银行/借记卡快捷接入/接口文档/光大借记卡快捷接口整理.xlsx");
+
+    List<Table> tables = new ArrayList<Table>();
+
+    public void read() throws InvalidFormatException, IOException {
+
+        OPCPackage pkg = OPCPackage.open("D:/www/work/G-光大银行/接口改造/报文字段整理.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(pkg);
-        
+
         int sheetsnum = wb.getNumberOfSheets();
-        
-        for(int i=0;i<sheetsnum;i++){
-            
+
+        for (int i = 0; i < sheetsnum; i++) {
             Sheet sheet = wb.getSheetAt(i);
-            
+
             String sheetName = sheet.getSheetName();
-            
+            Table newtable = new Table(sheetName);
+            tables.add(newtable);
             DataFormatter formatter = new DataFormatter();
+            int rownum = 0;
             for (Row row : sheet) {
+                rownum ++;
+                if(rownum == 1 )continue;
+                //判断是否为最后一空行
+                Cell fieldnameCell = row.getCell(1);
+                String lastrowcell = formatter.formatCellValue(fieldnameCell);
+                if(StringUtils.isBlank(lastrowcell)){
+                    break;
+                }
+                
+                Field newField = new Field();
+                newtable.addField(newField);
                 for (Cell cell : row) {
 
                     String text = formatter.formatCellValue(cell);
+                    int colummnIndex = cell.getColumnIndex();
+                    if (1 == colummnIndex) {
+                        newField.setName(text);
+                        newField.setComment(text);
+                    } else if (2 == colummnIndex) {
+                        newField.setType(text);
+                    }
 
                 }
+                
             }
-            
-            
-            
         }
-        
+
         pkg.close();
     }
+
+    public String getTableDml() {
+
+        StringBuilder sql = new StringBuilder();
+
+        for (Table tmptable : tables) {
+
+            sql.append("\nCREATE TABLE ");
+            sql.append(tmptable.getName()).append("(\n");
+
+            List<Field> fields = tmptable.getFields();
+            int nums = fields.size();
+            int i = 0;
+            for (Field field : fields) {
+                sql.append(field.getName()).append(" VARCHAR(32) ");
+                sql.append(" COMMENT '").append(field.getComment());
+                i++;
+                if(i<nums) sql.append("',\n");
+                else sql.append("'");
+            }
+
+            sql.append(");");
+        }
+
+        return sql.toString();
+    }
     
-    
+    public String printTableNames(){
+        StringBuilder sql = new StringBuilder();
+        
+        for (Table tmptable : tables) {
+            sql.append("<table tableName=\"").append(tmptable.getName()).append("\" />\n");
+        }
+        System.out.println(sql.toString());
+        return null;
+    }
+
+    class Field {
+
+        String name;
+        String comment;
+        String type;
+
+        public Field() {
+            super();
+        }
+
+        public Field(String name) {
+            super();
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+    }
+
+    class Table {
+
+        String      name;
+        List<Field> fields = new ArrayList<Field>();
+
+        public Table(String name) {
+            super();
+            this.name = name;
+        }
+
+        public void addField(Field field) {
+            this.fields.add(field);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<Field> getFields() {
+            return fields;
+        }
+
+        public void setFields(List<Field> fields) {
+            this.fields = fields;
+        }
+
+    }
+
     public static void main(String[] args) {
         ExcelUtils e = new ExcelUtils();
         try {
             e.read();
+            String sql = e.getTableDml();
+            System.out.println(sql);
+            
+            e.printTableNames();
         } catch (InvalidFormatException e1) {
             e1.printStackTrace();
         } catch (IOException e1) {
